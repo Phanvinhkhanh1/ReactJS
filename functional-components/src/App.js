9c3ea4bf0fa2680214870f3cbb99664289790922
Phan Vinh Khanh <Khanh161297@gmail.com>
2022-11-16 21:28:22 +0700

    State in ReactJs

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -2,18 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet';
 import Welcome from './component/Welcome';
+import Message from './component/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Phuong" job="Accountant" >
+      {/* <Greet name="Phuong" job="Accountant" >
         <p>This is my older sister</p>
       </Greet>
       <Greet name="Tung" job="Doctor" />
       <Greet name="Khanh" job="IT Engineer" />
       <Welcome name="Phuong" job="Accountant"></Welcome>
       <Welcome name="Tung" job="Doctor"></Welcome>
-      <Welcome name="Khanh" job="IT Engineer"></Welcome>
+      <Welcome name="Khanh" job="IT Engineer"></Welcome> */}
+      <Message/>
     </div>
   );
 }
